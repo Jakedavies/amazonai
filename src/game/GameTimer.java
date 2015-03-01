7266104077bb9d6c8d1b8323c3456cac1f8afbd6
jakedavies <jgdavies24@gmail.com>
2015-03-01 15:03:59 -0800

    simple gametimer built, not yet integrated into anything

--- src/game/GameTimer.java
+++ src/game/GameTimer.java
@@ -4,4 +4,18 @@ package game;
  * Created by jakedavies on 15-01-12.
  */
 public class GameTimer {
+
+        private long startTime;
+
+        public void start(){
+            startTime = System.currentTimeMillis();
+        }
+        public long msRemaining()
+        {
+            return System.currentTimeMillis() - startTime;
+        }
+        //less than 2 seconds left to make a move, time up
+        public boolean timeUp() {
+            return (System.currentTimeMillis() - startTime) < 2000;
+        }
 }
